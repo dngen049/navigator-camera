2d626a5b738ac6b9571bd72d9b62e82654b782a3
Diedrick Ng <dngen049@uottawa.ca>
2020-07-24 16:05:57 -0400

    Updates

---
+++ precache-manifest.2cdb91c17aeb056e5fda7711fcba0bda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f751ea25dde92aaa57ce9ff323e85de",
+    "url": "/navigator-camera/index.html"
+  },
+  {
+    "revision": "4e5903cb2197fdb71e57",
+    "url": "/navigator-camera/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "21d75e9ac517d89c7fba",
+    "url": "/navigator-camera/static/js/2.daf96888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/navigator-camera/static/js/2.daf96888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e5903cb2197fdb71e57",
+    "url": "/navigator-camera/static/js/main.c01356b2.chunk.js"
+  },
+  {
+    "revision": "fbc5fe2972d0603de67d",
+    "url": "/navigator-camera/static/js/runtime-main.aca07778.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/navigator-camera/static/media/logo.5d5d9eef.svg"
+  }
+]);
