2c7f50621f6ab71ba004e1cb660d176ca37e1dc7
Diedrick Ng <dngen049@uottawa.ca>
2020-07-24 16:14:25 -0400

    Added the permission code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const constraint = {
+    video: { width: 1280, height: 720 },
+    audio: false,
+  };
+  navigator.permissions.query({ name: "camera" }).then((result) => {
+    if (result.state === "granted") {
+      alert("Permission granted");
+    }
+    if (result.state === "prompt") {
+      navigator.getUserMedia(
+        constraint,
+        () => {
+          alert("Success");
+        },
+        (e) => {
+          alert("error", e);
+        }
+      );
+    }
+    if (result.state === "denied") {
+      alert("Permissions denied");
+    }
+  });
   return <div className="App">Welcome to my deploy</div>;
 }
 
